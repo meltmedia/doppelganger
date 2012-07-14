56d3a468d6c609576e7fcf40ba9275e2dbc3d5b6
Kyle W. Cartmell <KyleCartmel@gmail.com>
2012-07-13 18:24:42 -0700

    implemented basic proxy

--- bin/doppelgänger.js
+++ bin/doppelgänger.js
@@ -6,10 +6,31 @@
 /*jslint indent: 2, nomen: true*/
 
 var _ = require("underscore"),
-  http = require("http");
+  http = require("http"),
+  targetHost = "www.meltmedia.com",
+  targetPort = 80;
 
-console.log("### doppelgänger started");
+console.log("### doppelgänger starting...");
 
-http.createServer(function (request, response) {
-  response.end("doppelganger");
+http.createServer(function (inRequest, outResponse) {
+  var fixedHeaders = inRequest.headers;
+  fixedHeaders.host = targetHost + ":" + targetPort;
+  http.request({
+    "host": targetHost,
+    "port": targetPort,
+    "method": inRequest.method,
+    "path": inRequest.url,
+    "headers": fixedHeaders
+  }, function (inResponse) {
+    outResponse.statusCode = inResponse.statusCode;
+    _.each(inResponse.headers, function (value, key, list) { outResponse.setHeader(key, value); });
+    inResponse.on("data", function (chunk) {
+      outResponse.write(chunk);
+    });
+    inResponse.on("end", function () {
+      outResponse.end();
+    })
+  }).end();
 }).listen(8080, "0.0.0.0");
+
+console.log("### doppelgänger started!");
