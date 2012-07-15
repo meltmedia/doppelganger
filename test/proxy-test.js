24d08a4f683b34e3c4bf88a7801f617b48c428ea
Kyle W. Cartmell <KyleCartmel@gmail.com>
2012-07-14 23:47:04 -0700

    implemented a callback in the proxy for observed req-resp pairs

--- test/proxy-test.js
+++ test/proxy-test.js
@@ -1,5 +1,5 @@
 /**
- * doppelgänger-test.js
+ * proxy-test.js
  * Kyle W. Cartmell, Meltmedia (kyle.cartmell@meltmedia.com)
  **/
 "use strict";
@@ -27,7 +27,7 @@ describe("proxy", function () {
       return function (done) {
         helper.httpRequest({"host": "localhost", "port": proxyPort, "method": method,
           "path": "/ok"
-        }, function (response, data) {
+        }, "", function (response, data) {
           response.statusCode.should.equal(200);
           data.should.equal("ok");
           done();
@@ -39,11 +39,35 @@ describe("proxy", function () {
   it("should proxy 404 responses", function(done) {
     helper.httpRequest({"host": "localhost", "port": proxyPort, "method": "GET",
       "path": "/notfound"
-    }, function (response, data) {
+    }, "", function (response, data) {
       response.statusCode.should.equal(404);
       data.should.equal("not found");
       done();
     });
+  });
+
+});
+
+describe("proxy-with-callback", function () {
+
+  var app = express.createServer(),
+    proxyPort = 8081;
+  app.all('/ok', function(req, res){ res.send('ok'); });
+  app.listen(8080);
+  proxy.proxy({"targetHost":"localhost", "targetPort":8080, "proxyPort":proxyPort});
+
+  it("should invoke the sawRequestAndResponse callback on response", function(done) {
+    proxy.setSawRequestAndResponse(function (request, requestBody, response, responseBody) {
+      request.method.should.equal("POST");
+      request.url.should.equal("/ok");
+      requestBody.should.equal("hello");
+      response.statusCode.should.equal(200);
+      responseBody.should.equal("ok");
+      done();
+    });
+    helper.httpRequest({"host": "localhost", "port": proxyPort, "method": "POST",
+      "path": "/ok"
+    }, "hello", function (response, data) {});
   });
 
 });
