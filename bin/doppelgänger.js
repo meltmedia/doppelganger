943b5898ae2bcc6d2592a2d4271235930694edf6
Kyle W. Cartmell <KyleCartmel@gmail.com>
2012-07-17 18:06:14 -0700

    completed bcbs and gdata work - it is alive

--- bin/doppelgänger.js
+++ bin/doppelgänger.js
@@ -8,49 +8,65 @@
 var _ = require("underscore"),
   express = require("express"),
   fs = require("fs"),
+  md5 = require("md5"),
   observations = JSON.parse(fs.readFileSync("observations.json")),
-  app = express.createServer(/*{ "key": fs.readFileSync("ssl/key.pem"),
-  "cert": fs.readFileSync("ssl/cert.pem")}*/);
+  app = express.createServer(),
+  seriesTracker = 0,
+  secureApp = express.createServer({ "key": fs.readFileSync("ssl/key.pem"),
+  "cert": fs.readFileSync("ssl/cert.pem")}),
+  bodyReader = function(req, res, next) {
+    var buffer = "";
+    req.on("data", function (chunk) { buffer += chunk; });
+    req.on("end", function (chunk) { req.body = buffer; return next(); });
+  }, 
+  requestHandler = function(req, res){
+    var fo = observations;
+    // match method
+    var fo = _.filter(observations, function (o) { return o.request.method === req.method; });
+    // match url
+    fo = _.filter(fo, function (o) { return o.request.url === req.url; });
+    // match body
+    /*fo = _.filter(fo, function (o) {
+      var parseMethod = function (str) {
+        return _.filter(str.split("&"), function (n) { return n.indexOf("method=") !== -1; })[0].split("=")[1];
+      }
+      return parseMethod(o.requestBody) === parseMethod(req.body);
+    });*/
+    // try to find a 2XX response
+    //var fog = _.filter(fo, function (o) { return (o.response.statusCode > 199 && o.response.statusCode < 300); });
+    //if (fog.length > 0) { fo = fog; }
+    // serve the response, if there is one
+    if(fo.length > 0) {
 
-app.use(function(req, res, next) {
-  var buffer = "";
-  req.on("data", function (chunk) { buffer += chunk; });
-  req.on("end", function (chunk) { req.body = buffer; return next(); });
-});
-app.all('*', function(req, res){
-  var fo = observations;
+      // series advancement for bcbs
+      var obsbump = 0;
+      if(req.url === "/api/") {
+        obsbump += seriesTracker;
+        seriesTracker+=1;
+        console.log("obsbump === " + obsbump);
+      }
 
-
-
-  // match method
-  var fo = _.filter(observations, function (o) { return o.request.method === req.method; });
-  // match url
-  fo = _.filter(fo, function (o) { return o.request.url === req.url; });
-  // match body
-  fo = _.filter(fo, function (o) {
-    var parseMethod = function (str) {
-      return _.filter(str.split("&"), function (n) { return n.indexOf("method=") !== -1; })[0].split("=")[1];
+      console.log("found matching observation!");
+      var xo = fo[obsbump];
+      _.each(xo.response.headers, function (value, key, list) {
+        res.setHeader(key, value);
+      });
+      res.send(xo.responseBody,xo.response.statusCode);
+      console.log("REQUEST " + req.url + " - " + xo.requestBody);
+      console.log("RESPONSE " + res.statusCode + " - " + xo.responseBody);
+    } else {
+      console.log("cannot handle request");
+      res.send("WTF?",404);
     }
-    return parseMethod(o.requestBody) === parseMethod(req.body);
-  });
-  // try to find a 2XX response
-  var fog = _.filter(fo, function (o) { return (o.response.statusCode > 199 && o.response.statusCode < 300); });
-  if (fog.length > 0) { fo = fog; }
-  // serve the response, if there is one
-  if(fo.length > 0) {
-    console.log("found matching observation!");
-    var xo = fo[0];
-    _.each(xo.response.headers, function (value, key, list) {
-      res.setHeader(key, value);
-    });
-    res.send(xo.responseBody,xo.response.statusCode);
-  } else {
-    console.log("cannot handle request");
-    res.send("WTF?",404);
-  }
-});
+  };
 
+app.use(bodyReader);
+app.all("*", requestHandler);
 app.listen(80);
-//app.listen(443);
+
+secureApp.use(bodyReader);
+secureApp.all("*", requestHandler);
+secureApp.listen(443);
+
 
 console.log("### doppelgänger started");
